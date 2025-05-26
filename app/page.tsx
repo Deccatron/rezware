9684349852e1edfdc517b3c43ea2f1e0160eddb3
Dexter Sitwell <165820105+Deccatron@users.noreply.github.com>
2025-05-26 02:47:45 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function FullScreenIframe() {
                 <title>Rezware | Internal 89 UNC!</title>
             </Head>
             <iframe 
-                src="https://sociable-places-237386.framer.app" 
+                src="https://lovely-area-810581.framer.app" 
                 style={{ position: "fixed", top: 0, left: 0, width: "100vw", height: "100vh", border: "none" }}
                 allowFullScreen
             ></iframe>
